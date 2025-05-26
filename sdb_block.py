f731e1774e89e3c4d3fb14fe7beea5ecd8c6f8e8
TongaEast <166471914+Ru7139@users.noreply.github.com>
2025-05-26 13:15:29 +0800

    update

---
+++ sdb_block.py
@@ -0,0 +1,62 @@
+from surrealdb import Surreal
+#from surrealdb import AsyncSurreal
+
+# Using a context manger to automatically connect and disconnect
+with Surreal("ws://127.0.0.1:12345") as db:
+    db.signin({"username": 'ruut', "password": 'ruut'})
+    db.use("namepace_test", "database_test")
+
+    # Create a record in the person table
+    db.create(
+        "person",
+        {
+            "user": "me",
+            "password": "safe",
+            "marketing": True,
+            "tags": ["python", "documentation"],
+        },
+    )
+
+    # Read all the records in the table
+    print(db.select("person"))
+
+    # Update all records in the table
+    print(db.update("person", {
+        "user":"you",
+        "password":"very_safe",
+        "marketing": False,
+        "tags": ["Awesome"]
+    }))
+
+    # Delete all records in the table
+    print(db.delete("person"))
+
+    # You can also use the query method
+    # doing all of the above and more in SurrealQl
+
+    # In SurrealQL you can do a direct insert
+    # and the table will be created if it doesn't exist
+
+    # Create
+    db.query("""
+    insert into person {
+        user: 'me',
+        password: 'very_safe',
+        tags: ['python', 'documentation']
+    };
+    """)
+
+    # Read
+    print(db.query("select * from person"))
+
+    # Update
+    print(db.query("""
+    update person content {
+        user: 'you',
+        password: 'more_safe',
+        tags: ['awesome']
+    };
+    """))
+
+    # Delete
+    print(db.query("delete person"))
