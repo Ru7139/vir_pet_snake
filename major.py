7d470be90d6f1d4144c44d7f2cb88b7c1b941817
TongaEast <166471914+Ru7139@users.noreply.github.com>
2025-05-26 12:49:25 +0800

    first code

--- major.py
+++ major.py
@@ -1,5 +1,30 @@
+import collections
+import hashlib
+import math
+import os
+import random
+import re
+import shutil
+import sys
+import tarfile
+import time
+import zipfile
+from collections import defaultdict
+import pandas as pd
+import requests
+from IPython import display
+from matplotlib import pyplot as plt
+from matplotlib_inline import backend_inline
 
-a = 1
-b = 2
-c = a + b
-print(c)
+
+import numpy as np
+import torch
+import torchvision
+from PIL import Image
+from torch import nn
+from torch.nn import functional as F
+from torch.utils import data
+from torchvision import transforms
+
+x = torch.arange(12)
+print(x)
